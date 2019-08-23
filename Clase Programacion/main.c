bc670071193539a30a0399f7173ebfae3f581b79
Ramiro Paredes <52410319+Beowolf01@users.noreply.github.com>
2019-08-23 12:27:42 -0300

    Add files via upload

---
+++ Clase Programacion/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+int main()
+{
+    int edad;
+    float altura;
+    char sexo;
+    char seguir;//Solo necesito inicializarla si es un while solo y no un do while.
+    int edadMujerMasVieja;
+    float alturaMujerMasVieja;
+    int edadMin;
+    float alturaPersonaMasJoven;
+    int edadHombreMasBajo;
+    float minimaAltura;
+    int acumuladorDeEdades=0;
+    int contadorPersonas=0;
+    float promedioEdades;
+    int flagPetiso = 0;
+    int flagPersonaJoven=0;
+    int flagVieja=0;
+
+
+    do
+    {
+        printf("Ingrese la edad: ");
+        scanf("%d", &edad);
+        while(edad<0 || edad>100)
+        {
+            printf("Reingrese la edad: ");
+            scanf("%d", &edad);
+        }
+        printf("Ingrese la altura: ");
+        scanf("%f", &altura);
+        while(altura<1 || altura>2)
+        {
+            printf("Reingrese la altura: ");
+            scanf("%f", &altura);
+        }
+        printf("Ingrese sexo (f/m): ");
+        sexo = getche();
+        sexo = tolower(sexo);
+        while(sexo!='f' && sexo!='m');
+        {
+            printf("\nReingrese sexo: ");
+            sexo = getche();
+            sexo = tolower(sexo);
+        }
+        if(sexo=='f')
+        {
+            if(flagVieja == 0 || edad>edadMujerMasVieja)
+            {
+                edadMujerMasVieja = edad;
+                alturaMujerMasVieja = altura;
+                flagVieja = 1;
+            }
+        }
+        else
+        {
+            if(flagPetiso=0 || altura<minimaAltura)
+            {
+                minimaAltura = altura;
+                edadHombreMasBajo = edad;
+                flagPersonaJoven = 1;
+            }
+        }
+
+        if(flagPersonaJoven==0 || edad<edadMin)
+        {
+            edadMin = edad;
+            alturaPersonaMasJoven = altura;
+            flagPersonaJoven = 1;
+        }
+
+        contadorPersonas++;
+        acumuladorDeEdades = acumuladorDeEdades + edad;
+
+        printf("\nDesea continuar? (s/n): ");
+        fflush(stdin);
+        scanf("%c", &seguir);
+
+    }while(seguir == 's');
+    promedioEdades = (float)acumuladorDeEdades/contadorPersonas;
+
+    printf("La altura de la mujer mas vieja es: %f\n", alturaMujerMasVieja);
+    printf("La altura de la persona mas joven es: %f\n", alturaPersonaMasJoven);
+    printf("La edad del hombre mas bajo es: %d\n", edadHombreMasBajo);
+    printf("El promedio es: %f", promedioEdades);
+    return 0;
+}
